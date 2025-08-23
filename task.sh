7f05143d4bd94a3b4a6809e8306b8faa434343ee
ktw <827824740@qq.com>
2025-08-23 09:23:04 +0800

    1

--- task.sh
+++ task.sh
@@ -2,7 +2,4 @@
 # cron:0 0 * * *
 # new Env('bili每日直播任务');
 
-
-target_task_code="LiveLottery"
-run_task "${target_task_code}"
-# task myktw_BiliBiliLiveToolsHelper/fansMedalHelper/index.py
+task myktw_BiliBiliLiveToolsHelper/fansMedalHelper/index.py
