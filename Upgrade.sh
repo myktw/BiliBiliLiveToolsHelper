1f44fda5d0caf3409fe1a1ea6c8214baa787d000
ktw <827824740@qq.com>
2025-08-23 08:26:28 +0800

    w

---
+++ Upgrade.sh
@@ -0,0 +1,5 @@
+git clone https://github.com/XiaoMiku01/fansMedalHelper.git
+# cp fansMedalHelper/users.example.yaml fansMedalHelper/users.yaml
+cd fansMedalHelper
+git config --global --add safe.directory /ql/data/scripts/BiliBiliLive/fansMedalHelper
+git pull
---
+++ task.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+# cron:5 9 * * *
+# new Env('bili每日直播任务');
+
+. update_fansMedalHelper.sh
